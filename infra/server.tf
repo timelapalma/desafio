578441b4bee249240909d1f3f5cb13135f9d49cc
Helder Pereira <profhelder.pereira@fiap.com.br>
2024-07-09 19:38:55 -0300

    Ajsutando modelo com uso de um ALB para o Grafana e simplificando a DOC

--- infra/server.tf
+++ infra/server.tf
@@ -29,6 +29,8 @@ resource "aws_autoscaling_group" "lab" {
   }  
 }
 
+### Application Server ALB
+
 resource "aws_lb" "server" {
   name               = "asg-server-lb"
   internal           = false
@@ -59,4 +61,38 @@ resource "aws_lb_target_group" "server" {
 resource "aws_autoscaling_attachment" "server" {
   autoscaling_group_name = aws_autoscaling_group.lab.id
   alb_target_group_arn   = aws_lb_target_group.server.arn
+}
+
+### Grafana ALB
+
+resource "aws_lb" "grafana" {
+  name               = "asg-grafana-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.asg_lab_lb.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_listener" "grafana" {
+  load_balancer_arn = aws_lb.grafana.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.grafana.arn
+  }
+}
+
+resource "aws_lb_target_group" "grafana" {
+  name     = "asg-grafana"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+
+resource "aws_autoscaling_attachment" "grafana" {
+  autoscaling_group_name = aws_autoscaling_group.lab.id
+  alb_target_group_arn   = aws_lb_target_group.grafana.arn
 }
